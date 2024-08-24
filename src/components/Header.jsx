8144781b893363dbce9ed66a40142398bbc603e8
Parvez Mahamud AA <parvezmahmudaa100@gmail.com>
2024-08-24 19:22:39 +0600

    Completed Clerk Authentication Set up

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react"
 import { Link } from "react-router-dom"
-import { Button } from "./ui/button"
 
 const Header = () => {
   return (
@@ -9,7 +9,13 @@ const Header = () => {
                 <img src="../../public/logo.png" className="h-20" />
             </Link>
 
-            <Button variant="ouline">Login</Button>
+            {/* <Button variant="ouline">Login</Button> */}
+            <SignedOut>
+              <SignInButton />
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
         </nav>
     </>
   )
