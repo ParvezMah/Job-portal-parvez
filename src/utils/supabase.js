c1ad584700d1f2862a778c50aaec28cc69300a7b
Parvez Mahamud AA <parvezmahmudaa100@gmail.com>
2024-08-24 19:29:51 +0600

    Connected Supabase with Clerk

--- src/utils/supabase.js
+++ src/utils/supabase.js
@@ -5,8 +5,15 @@ const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 const supabaseClient = async(supabaseAccessToken)=> {
-    const supabase = createClient(supabaseUrl, supabaseKey);
+    const supabase = createClient(supabaseUrl, supabaseKey, {
+        global: {
+            headers: {
+                Authorization: `Bearer ${supabaseAccessToken}`
+            }
+        }
+    });
+    return supabase;
 }
 
-export default supabase
-        
+export default supabaseClient;
+         
