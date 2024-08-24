d2828779d6ba678f15d46d6970c2bc81551c33e9
Parvez Mahamud AA <parvezmahmudaa100@gmail.com>
2024-08-24 20:29:18 +0600

    Completed Landing Page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react"
 import { Link } from "react-router-dom"
+import { Button } from "./ui/button"
 
 const Header = () => {
   return (
@@ -9,13 +10,13 @@ const Header = () => {
                 <img src="../../public/logo.png" className="h-20" />
             </Link>
 
-            {/* <Button variant="ouline">Login</Button> */}
-            <SignedOut>
+            <Button variant="ouline">Login</Button>
+            {/* <SignedOut>
               <SignInButton />
             </SignedOut>
             <SignedIn>
               <UserButton />
-            </SignedIn>
+            </SignedIn> */}
         </nav>
     </>
   )
